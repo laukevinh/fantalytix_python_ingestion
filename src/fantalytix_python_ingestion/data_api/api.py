84f0bd8bdb9869e4ecc78e7aed4559cbd5a56782
Kevin Lau <laukevinh@gmail.com>
2019-02-23 22:39:17 +0000

    Merged in add_cors (pull request #9)
    Add initial CORS

--- src/fantalytix_python_ingestion/data_api/api.py
+++ src/fantalytix_python_ingestion/data_api/api.py
@@ -7,21 +7,28 @@ from fantalytix_sqlalchemy.orm.common import League
 
 from .db import get_db
 
+from .utils import crossdomain
+
 from .schema import LeagueSchema
 
 league_schema = LeagueSchema(strict=True)
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/leagues', methods=['GET'])
+@crossdomain(origin='http://localhost:3000')
 def leagues():
     results = league_schema.dump(get_db().query(League).all(), many=True)
 
     return jsonify(results.data)
 
 @bp.route('/leagues/abbreviation/<string:abbreviation>', methods=['GET'])
+@crossdomain(origin='http://localhost:3000')
 def leagues_abbreviation(abbreviation):
-    results = league_schema.dump(
-        get_db().query(League).filter_by(abbreviation=abbreviation).first()
-    )
+    try:
+        results = league_schema.dump(
+            get_db().query(League).filter_by(abbreviation=abbreviation).first()
+        )
+    except AttributeError:
+        return jsonify({})
 
     return jsonify(results.data)
